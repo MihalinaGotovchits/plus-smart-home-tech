60a1fa8b326d81f40f924ebc918815aa4ca3139f
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-01-28 23:36:14 +0300

    Перенесла серилизатор в collector и переименовала его (так понятнее что это за класс =) )

--- telemetry/collector/src/main/java/ru/yandex/practicum/avro/AvroSerializer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/avro/AvroSerializer.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.kafka;
+package ru.practicum.avro;
 
 import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DatumWriter;
@@ -11,7 +11,7 @@ import org.apache.kafka.common.serialization.Serializer;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class GeneralKafkaSerializer implements Serializer<SpecificRecordBase> {
+public class AvroSerializer implements Serializer<SpecificRecordBase> {
 
     private final EncoderFactory encoderFactory = EncoderFactory.get();
     private BinaryEncoder encoder;
