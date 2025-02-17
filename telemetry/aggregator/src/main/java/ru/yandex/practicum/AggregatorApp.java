282e43dd208b7925638a28a3d6bd4a14290fcd88
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-02-17 22:44:38 +0300

    Исправила опечатки

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregatorApp.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregatorApp.java
@@ -10,7 +10,5 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class AggregatorApp {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(AggregatorApp.class, args);
-
-
     }
 }
