7352ec5aa97de77d12fbe89483245af218654077
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-03-11 20:03:12 +0300

    Изменила конфиги, добавила аннотации

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApp.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApp.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.yandex.practicum.processor.HubEventProcessor;
 import ru.yandex.practicum.processor.SnapshotProcessor;
 
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AnalyzerApp {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApp.class, args);
